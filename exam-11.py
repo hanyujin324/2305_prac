fde87ae0bd641d9cfaa2de15d13284ba493820d4
한유진 <hanyujin32432@gmail.com>
2023-05-01 22:53:03 +0900

    exam11 python해석

--- exam-11.py
+++ exam-11.py
@@ -1,8 +1,8 @@
 example=[[1,2,3],[4,[5,6]],7,[8,9]]
-def flatten(data);
+def flatten(data); #flatten: 다차원 배열을 1차원으로 바꿔줌
 output=[]
 for item in data:
-  if type(item)==list:
+  if type(item)==list: #item이 리스트인지 확인
     #재귀적으로 처리
     output+=flatten(item)
   else:
