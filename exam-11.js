496fb5165ecf210c520aae728ae26f88cd1928aa
한유진 <hanyujin32432@gmail.com>
2023-05-01 23:04:37 +0900

    flatten과 output을 합치는 것 빼고 작성

--- exam-11.js
+++ exam-11.js
@@ -13,4 +13,18 @@ for item in data:
     output.append(item)
     return output
   print(flatten(example)) */
-  let example=[[1,2,3],[4,[5,6]],7,[8,9]]
+  let example=[[1,2,3],[4,[5,6]],7,[8,9]];
+  function flatten(data); //중첩된 배열을 하나로 만들어 준다.
+  let output=[]
+  for (item in data){
+/*     if(typeof item==list){ } javascript에서는 typeof로 배열인지 확인이 불가능*/
+    if(Array.isArray(item)){ //배열인지 확인하기위해 isArray를 사용해야한다.
+      output+=flatten(item); //이게 무슨 뜻일까?
+    }else{
+      output.push(item);
+    }
+    return output;
+  }
+console.log(flatten(example));
+
+
