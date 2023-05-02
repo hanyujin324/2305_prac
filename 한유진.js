cba4c6a591830d7fed79f4b19b89e110d76b4d7e
한유진 <hanyujin32432@gmail.com>
2023-05-02 22:50:31 +0900

    모듈

--- 한유진.js
+++ 한유진.js
@@ -3,7 +3,6 @@ const utility=require('./exam-12');
 const a=10;
 const b=20;
 
-console.log(add(a,b)); //여기에 utility를 넣는 방법은?
-console.log(minus(a,b));
-console.log(divide(a,b));
-console.log(multiply(a,b));
+console.log(utility.add(a,b)); 
+console.log(utility.divide(a,b));
+console.log(utility.multiply(a,b));
