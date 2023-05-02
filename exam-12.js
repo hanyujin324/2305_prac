6ed83083dd6dec1316ffb1189e1be3fc5751ef55
한유진 <hanyujin32432@gmail.com>
2023-05-02 22:48:55 +0900

    Q.1 하나의 클래스 형태로 모듈 제작

--- exam-12.js
+++ exam-12.js
@@ -17,7 +17,7 @@ console.log(multiply(a,b)); */
 위에 있는 4개는 각각 덧셈, 뺄셈, 나눗셈, 곱셈이 들어있는 모듈이다.
 
 덧셈과 뺄셈 등이 들어있는 모듈을 만들려면 어떻게 해야할까? */
-class utility{
+export class utility{
   add(a,b){
     return a+b;
   }
@@ -31,7 +31,6 @@ class utility{
     return a*b;
   }
 }
-
 /* Q.1 사칙연산 모듈을 4개 만들어서 import(가져오기)후 호출한 예시
 Q.1 아무래도 이렇게 하나하나 가져오는 것은 가독성면에서나 사용성면에서 좋아보이지 않는다.
 Q.1 이것을 하나의 Class 형태의 모듈로 만들어 관리할 수 있을까요? */
