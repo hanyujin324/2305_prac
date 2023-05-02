e57a3890b810f1a5594c2beac91d9a3360e47ced
한유진 <hanyujin32432@gmail.com>
2023-05-02 22:36:44 +0900

    모듈을 불러와서 콘솔에 출력

---
+++ 한유진.js
@@ -0,0 +1,9 @@
+const utility=require('./exam-12');
+
+const a=10;
+const b=20;
+
+console.log(add(a,b)); //여기에 utility를 넣는 방법은?
+console.log(minus(a,b));
+console.log(divide(a,b));
+console.log(multiply(a,b));
